8cb0bb239aa4c83138e8d19b1028046d80454563
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2026-03-18 02:21:47 +0100

    Add doc build script

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+OUT=out
+FILE_NAME='main.typ'
+
+mapfile -t FILES < <(find . -name "$FILE_NAME")
+
+if ! command -v typst >/dev/null 2>&1; then
+    echo "Error: 'typst' command not found." >&2
+    exit 1
+fi
+
+mkdir -p "$OUT"
+
+for file in "${FILES[@]}"; do
+    echo "Processing $file"
+    DIR_NAME=$(dirname "$file")
+    DIR_NAME=${DIR_NAME#./}
+    DIR_NAME=${DIR_NAME//\//-}
+    OUT_NAME="${OUT}/${DIR_NAME}.pdf"
+    echo "Output name: $OUT_NAME"
+    typst compile "$file" "$OUT_NAME"
+done
